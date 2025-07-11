046135efb661583cbd77a940a86555d384d1131f
Nono-M <issmar+github@gmail.com>
2025-07-12 00:00:52 +0200

    File structure

---
+++ scripts/global.gd
@@ -0,0 +1,43 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func red_flame():
+	pass
+
+
+func blue_flame():
+	pass
+
+
+func yellow_flame():
+	pass
+
+
+func load_word():
+	pass
+
+
+func load_flames():
+	pass
+
+
+func reset_puzzle():
+	pass
+
+
+func calculate_score():
+	pass
+
+
+func timer():
+	pass
---
+++ scripts/main.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var key_word = "consume"
+var red_flames = 3
+var blue_flames = 0
+var yellow_flames = 0
+var answers = ["come","cone","cons"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
