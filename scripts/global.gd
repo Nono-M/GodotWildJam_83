6f957c388d94eda003a754a4123f2178f8534d6f
Nono-M <issmar+github@gmail.com>
2025-07-12 11:10:40 +0200

    Added load_keyword function and letter child instances

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,16 +1,5 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func red_flame():
 	pass
 
@@ -23,8 +12,14 @@ func yellow_flame():
 	pass
 
 
-func load_word():
-	pass
+func load_word(keyword):
+	var letter_scene = preload("res://letter.tscn") 
+	for character:int in keyword.length():
+		var letter = letter_scene.instantiate()
+		letter.get_node("Label").text = keyword[character]
+		letter.name = "Letter%d" % character
+		get_tree().root.get_node("Main/WordDisplayer").add_child(letter)
+
 
 
 func load_flames():
