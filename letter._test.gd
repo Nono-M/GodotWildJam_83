0e1f252b1e19154d412e7b27e1ff82aa1f58cd2f
Nono-M <issmar+github@gmail.com>
2025-07-19 11:20:31 +0200

    Test setup and files for letter burning shader effect

---
+++ letter._test.gd
@@ -0,0 +1,37 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#var text_mesh = TextMesh.new()
+	#text_mesh.font = $Label.label_settings.font
+	#text_mesh.font_size = 64
+	#text_mesh.text = "W"
+	#
+	#$MeshInstance2D.mesh.text = $Label.text
+	
+	var font = get_tree().root.get_theme_default_font()
+	var font_rid = font.get_rids()[0]
+	var text_server = TextServerManager.get_primary_interface()
+	var glyph_index = text_server.font_get_glyph_index(font_rid, 80, "M".unicode_at(0), 0)
+	var glyph_offset = text_server.font_get_glyph_offset(font_rid,Vector2i(80,80),glyph_index)
+	print(glyph_offset)
+	
+	$MeshInstance2D.mesh.offset = Vector2(-glyph_offset.x , glyph_offset.y)
+	$MeshInstance2D.position = $Label.position
+	#print("Mesh pos : %s" % $MeshInstance2D.mesh.get_glyph_index_at_line_column(0,0))
+	#print("Label pos : %s" % $Label.label_settings.font.get_glyphe_offset())
+	#burn_letter_animation()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#$MeshInstance2D.material.set_shader_parameter("burn", delta * 0.2)
+	#print($MeshInstance2D.mesh.material.get_shader_parameter("burn"))
+	pass
+
+
+func burn_letter_animation():
+	var burn_tween = get_tree().create_tween()
+	burn_tween.tween_property($MeshInstance2D,"material:shader_parameter/burn",1.0, 1.0)
