9fb0e5e3f49c70caefe978e71e17d91ce171b06e
Nono-M <issmar+github@gmail.com>
2025-07-23 23:25:34 +0200

    Hint system using online dictionary api + colorblind colors for buttons

--- scripts/interface.gd
+++ scripts/interface.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var http_request:HTTPRequest
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,3 +33,25 @@ func _on_texture_button_pressed():
 func _on_help_button_pressed():
 	$HelpContainer.visible = true
 	Audio.get_node("ClickSound").play()
+
+
+func _on_hint_button_pressed():
+	Audio.get_node("ClickSound").play()
+	if Data._answers.size() > 0 :
+		var hint_word:String = Data._answers.pick_random()
+		http_request = HTTPRequest.new()
+		add_child(http_request)
+		http_request.request_completed.connect(display_definition)
+		
+		var error = http_request.request("https://api.dictionaryapi.dev/api/v2/entries/en/%s" % hint_word)
+		if error != OK:
+			push_error("HTTP request to dictionary api failed")
+
+func display_definition(result, response_code, headers, body):
+	var response_dictionary:Dictionary = JSON.parse_string(body.get_string_from_utf8())[0]
+	var random_response_meaning:Dictionary = response_dictionary["meanings"][ randi_range(0,response_dictionary["meanings"].size())-1 ]
+	var random_response_definition:String = random_response_meaning["definitions"][ randi_range(0,random_response_meaning["definitions"].size()-1) ]["definition"]
+	
+	Global.display_message("Hint : \n%s" % random_response_definition)
+	
+	http_request.queue_free()
